00ee194dcf77aff7dccac77aed75d80fd2c8c614
Alexa Martinez <alexamirabelle@gmail.com>
2022-06-11 00:25:54 +0800

    cocktail controller

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,6 @@
 class Cocktail < ApplicationRecord
 
-  validates :name, presence: true, uniquness: true #must have a unique name (thus name is present).
+  validates :name, presence: true, uniqueness: true #must have a unique name (thus name is present).
 
 
   has_many :measurements, dependent: :destroy  #A cocktail has many doses, #When you delete a cocktail, you should delete associated doses (but not the ingredients as they can be linked to other cocktails).
