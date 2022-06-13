ac55823549d67b93b25ac27fc8a407b1f4fcd82f
Alexa Martinez <alexamirabelle@gmail.com>
2022-06-14 00:10:41 +0800

    font

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
-  resources :cocktails, [:show, :edit, :udpate, :destroy]
+  resources :cocktails, only: [:index, :new, :show, :edit, :udpate, :destroy]
 end
